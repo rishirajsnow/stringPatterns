bca3b442be479d39920cae016b32bc67b06e89c0
rishiraj Bhowmik <rishiraj.bhowmik5@gmail.com>
2024-10-22 01:06:17 +0530

    z-Algo new problems added.

---
+++ BinA.js
@@ -0,0 +1,7 @@
+// Give a string A (|A|=N) & a string B (|B|=M). (N>M)
+// Find the number of times B is present in A.
+let A="abaccababac";
+let B="aba";
+//Ans=3  (Expected)
+//concat B and A -> B.concat(A)
+//use Z-algo to find the solution-> count of every i th element in z array greater than equal to length of B.length 
---
+++ smallestPeriod.js
@@ -0,0 +1,7 @@
+//Find the smallest period of a string.
+// Smallest integer k s.t [for all i ,s[i]==s[i%k]].
+
+let s="abcaabcaabcaab"// Expected Ans->4
+
+//Find the Z array using Z-Algo
+//smallest i=> (i+z[i]===N) => Ans=i
